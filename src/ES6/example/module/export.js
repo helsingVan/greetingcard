0b3930bc1aae4689438cb986d93bf50f60265a92
helsingvan <it.fan@foxmail.com>
2017-06-28 17:04:17 +0800

    es6

---
+++ src/ES6/example/module/export.js
@@ -0,0 +1,7 @@
+let a = 'a';
+let b = [1,2,3];
+let c = function() {
+	return 'this is a func';
+};
+
+export { a,b,c };
---
+++ src/ES6/example/module/main.js
@@ -0,0 +1,6 @@
+import { a,b,c } from './export.js';
+
+console.log('success');
+console.log(a);
+console.log(b);
+console.log(c);
