ccbc623b3767d068d329e8b9b7e506411f27ce50
helsingvan <it.fan@foxmail.com>
2017-08-21 18:15:04 +0800

    canvas

--- src/fish/js/class.js
+++ src/fish/js/class.js
@@ -6,20 +6,20 @@ function Haikui() {
 }
 Haikui.prototype.init = function() {
   for(var i=0;i<this.num;i++) {
-  	this.x[i] = i * 15 + Math.random() * 20;
+  	this.x[i] = i * 16 + Math.random() * 20;
   	this.y[i] = 100 + Math.random() * 50;
   }
 };
 Haikui.prototype.draw = function() {
   ctx2.save();
-  ctx2.globalAlpha = 1;
+  ctx2.globalAlpha = 0.5;
   ctx2.lineWidth = 20;
   ctx2.lineCap = 'round';
-  ctx2.strokeStyle = '#fff';
+  ctx2.strokeStyle = '#ff7800';
   for(var i=0;i<50;i++) {
   	ctx2.beginPath();
   	ctx2.moveTo(this.x[i],canHeight);
-  	ctx2.lineTo(this.y[i],canHeight - this.y[i]);
+  	ctx2.lineTo(this.x[i],canHeight - this.y[i]);
   	
   	ctx2.stroke();
   }
@@ -31,6 +31,7 @@ function FruitClass() {
   this.alive = [];
   this.x = [];
   this.y = [];
+  this.l = [];
   this.orange = new Image();
   this.blue = new Image();
 }
@@ -46,17 +47,27 @@ FruitClass.prototype.init = function() {
 };
 FruitClass.prototype.draw = function() {
   var self = this;
-  for(var i=0;i<this.num;i++) {
-    this.orange.onload = function() {
+  for(var i=0;i<self.num;i++) {
+    if(self.l[i] < 14) {
+      self.l[i] += 0.01*deltaTime;
       
-      ctx2.drawImage(self.orange,10,10);
-      
+    }else {
+      // this.y[i] -= 0.1 * deltaTime;
     }
-    
+    ctx2.drawImage(self.orange,
+        self.x[i] - self.l[i]*0.5,
+        self.y[i] - self.l[i],
+        self.l[i],
+        self.l[i]);
+    ctx2.clearRect(self.x[i]-self.l[i]*0.5,
+      self.y[i],
+      self.l[i],
+      self.l[i]);
   }
 };
 FruitClass.prototype.born = function(i) {
   var haikuiId = Math.floor(Math.random() * haikui.num);
   this.x[i] = haikui.x[haikuiId];
   this.y[i] = canHeight - haikui.y[haikuiId];
+  this.l[i] = 0;
 }
--- src/fish/js/main.js
+++ src/fish/js/main.js
@@ -4,9 +4,9 @@ var can2 = document.querySelector('#c2');
 var ctx1 = can1.getContext('2d');
 var ctx2 = can2.getContext('2d');
 var canHeight = can2.clientHeight;
-console.log(canHeight);
-// var lastTime = Date.now();
-// var deltaTIme = 0;
+var lastTime = Date.now();
+var deltaTIme = 0;
+
 var haikui = new Haikui();
 haikui.init();
 var fruit = new FruitClass();
@@ -16,27 +16,23 @@ console.log(fruit);
 var game = {
 	init: function() {
 	  
-	  var backgroundImage = new Image();
-	  backgroundImage.src = './image/background.jpg';
-
-	  backgroundImage.onload = function() {
-	  	ctx2.drawImage(backgroundImage,0,0);
-	  }
-
-
+	  haikui.draw();
 	  gameLoop();
 	}
 }
 
 // 帧绘制
 function gameLoop() {
-  // var now = Date.now();
-  // deltaTime = now - lastTime;
-  // lastTime = now;
-
+  
+  var now = Date.now();
+  deltaTime = now - lastTime;
+  lastTime = now;
+  
   fruit.draw();
- 
-  // requestAnimFrame(gameLoop);
+
+  requestAnimFrame(gameLoop);
+
+
 }
 
 
